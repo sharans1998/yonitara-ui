46b55c3999cedaceca9ccc827d0212e788c267d4
Sharan <sharan1998satish@gmail.com>
2024-11-01 17:53:22 +0530

    remove unnecessary files.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,16 +1,27 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
+      fontFamily: {
+        sans: ['var(--font-poppins)'],
+        mono: ['var(--font-roboto-mono)'],
+      },
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        black: '#010101',
+        lightGray: '#bdbdbd',
+        gray: '#7b7b7b',
+        darkGray: '#2d2d2d',
+        white: '#f8f8f7',
+        primary: '#8d31be',
+        secondary: '#ffede5',
+        buttonPrimary: '#f8d958',
+        buttonSecondary: '#e96e2a',
       },
     },
   },
