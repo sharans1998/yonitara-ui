d83e82d547467cf1ff9eaa061be93dc13e9f8e9e
Sharan <sharan1998satish@gmail.com>
2024-11-01 20:53:23 +0530

    setup actions.

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'export', // <=== enables static exports
+  reactStrictMode: true,
+  basePath: '/yonitara-ui',
 };
 
 export default nextConfig;
