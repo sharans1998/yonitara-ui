799b71ba1d52d4f75c612cbc768d955fb9c79a60
Sharan <sharan1998satish@gmail.com>
2024-11-01 23:23:23 +0530

    add basepath.

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,7 @@ const nextConfig: NextConfig = {
   /* config options here */
   output: 'export', // <=== enables static exports
   reactStrictMode: true,
+  basePath: '/yonitara-ui',
   images: {
     unoptimized: true,
     loader: 'custom',
