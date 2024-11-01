fa4af85a50c7399118481ec1429675d6b1d1d737
Sharan <sharan1998satish@gmail.com>
2024-11-02 00:18:48 +0530

    footer optimised.

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { FaFacebook, FaInstagram } from 'react-icons/fa';
 import { FaXTwitter } from 'react-icons/fa6';
+import ContentWrapper from './ContentWrapper';
 
 const Footer: React.FC = () => {
   return (
-    <footer className="bg-black text-white py-6 h-60">
-      <div className="container mx-auto">
+    <footer className="bg-black text-white py-6 h-60 px-4">
+      <div className="max-w-7xl mx-auto">
         <div className="flex justify-between items-center mb-4">
           <div>
             <img src="/logo.svg" alt="Logo" className="h-10" />
@@ -37,7 +38,7 @@ const Footer: React.FC = () => {
             </a>
           </div>
         </div>
-        <div className="flex justify-between items-center border-t border-gray-700 pt-4">
+        <div className="flex flex-col gap-4 md:flex-row justify-between items-center border-t border-gray-700 pt-4 text-base">
           <p>&copy; 2023 Your Company. All rights reserved.</p>
           <div className="flex space-x-4">
             <a href="/privacy-policy" className="hover:text-gray-400">
