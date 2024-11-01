51a4e7a3359bcde8423c93b6f68844ad057aa921
Sharan <sharan1998satish@gmail.com>
2024-11-01 22:11:44 +0530

    fix images.

--- next.config.ts
+++ next.config.ts
@@ -4,20 +4,8 @@ const nextConfig: NextConfig = {
   /* config options here */
   output: 'export', // <=== enables static exports
   reactStrictMode: true,
-  basePath: '/yonitara-ui',
   images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'sharans1998.github.io',
-        pathname: '/yonitara-ui/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'sharans1998.github.io',
-        pathname: '/yonitara-ui/**/*',
-      },
-    ],
+    unoptimized: true,
   },
 };
 
