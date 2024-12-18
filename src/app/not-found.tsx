a61ac966cbf7c77632e479484ddee6ada894c305
Sharan Satish <sharansatish@Sharans-MacBook-Pro.local>
2024-12-18 21:53:00 +0530

    fix not found

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -4,13 +4,15 @@ import Section from '@/components/layout/Section';
 export default function NotFound() {
   return (
     <Section>
-      <div className="flex flex-col gap-5 text-center">
+      <div className="mx-auto flex flex-col gap-5 text-center lg:max-w-lg">
         <h2 className="text-4xl">Lost in Space</h2>
         <p className="mb-6">
           Oops! The page you&apos;re looking for has wandered off. <br />
           Try heading back home!
         </p>
-        <Button variant="tertiary" href="/" label="Go Home" />
+        <div className="flex justify-center">
+          <Button variant="tertiary" href="/" label="Go Home" />
+        </div>
       </div>
     </Section>
   );
