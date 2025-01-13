4a1510ff3ffdb88dbe1c0414ca7af3eb50cf934f
Sharan <sharan1998satish@gmail.com>
2025-01-13 11:44:20 +0530

    update vimeo video.

--- src/components/common/VimeoPlayer.tsx
+++ src/components/common/VimeoPlayer.tsx
@@ -5,10 +5,22 @@ import React, { Suspense } from 'react';
 const VimeoPlayer = ({ videoId }: { videoId: string }) => {
   return (
     <Suspense fallback={<div>Loading...</div>}>
-      <video className="my-4 h-auto w-full" controls preload="none">
-        <source src={videoId || '/path/to/video.mp4'} type="video/mp4" />
-        Your browser does not support the video tag.
-      </video>
+      <div style={{ padding: '56.25% 0 0 0', position: 'relative' }}>
+        <iframe
+          src="https://player.vimeo.com/video/1046228216?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479"
+          frameBorder="0"
+          allow="autoplay; fullscreen; picture-in-picture; clipboard-write; encrypted-media"
+          style={{
+            position: 'absolute',
+            top: 0,
+            left: 0,
+            width: '100%',
+            height: '100%',
+          }}
+          title="Yoni Natal Sutra"
+        ></iframe>
+      </div>
+      <script src="https://player.vimeo.com/api/player.js"></script>
     </Suspense>
   );
 };
