d7bd5a79980427cba0f31be044ad874d20f5bae8
Sharan <sharan1998satish@gmail.com>
2025-02-27 14:15:45 +0530

    Update Pixel code.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -34,18 +34,17 @@ export default function RootLayout({
         <script
           dangerouslySetInnerHTML={{
             __html: `
-              !function(f,b,e,v,n,t,s)
-              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?                         
-              n.callMethod.apply(n,arguments):n.queue.push   
-              (arguments)}; if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!
-              0;n.version='2.0';n.queue=[];t=b.createElement(e);
-              t.async=!0;t.src=v;s=b.getElementsByTagName(e)[0];
-              s.parentNode.insertBefore(t,s)}(window, document,
-              'script',
-              'https://connect.facebook.net/en_US/fbevents.js');
-              fbq('init', process.env.NEXT_PUBLIC_FACEBOOK_PIXEL_ID);
-              fbq('track', 'PageView');
-            `,
+      !function(f,b,e,v,n,t,s)
+      {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+      n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+      if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+      n.queue=[];t=b.createElement(e);t.async=!0;
+      t.src=v;s=b.getElementsByTagName(e)[0];
+      s.parentNode.insertBefore(t,s)}(window, document,'script',
+      'https://connect.facebook.net/en_US/fbevents.js');
+      fbq('init', '9198949973532518');
+      fbq('track', 'PageView');
+    `,
           }}
         />
         <noscript>
@@ -53,8 +52,7 @@ export default function RootLayout({
             height="1"
             width="1"
             style={{ display: 'none' }}
-            src={`https://www.facebook.com/tr?id=${process.env.NEXT_PUBLIC_FACEBOOK_PIXEL_ID}&ev=
-            PageView&noscript=1`}
+            src="https://www.facebook.com/tr?id=9198949973532518&ev=PageView&noscript=1"
           />
         </noscript>
         {/* End Meta Pixel Code */}
